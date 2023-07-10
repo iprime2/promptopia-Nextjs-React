05832d114eb50e50ed3ec4c94530537eec70cedc
Sushil Gupta <kalwarsushil2@gmail.com>
2023-07-10 16:18:43 +0545

    deleting unnecessary from create prompt page and add empty fragment tag to wrap the whole component

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,47 +1,6 @@
 import Form from '@components/Form'
-// import { useSession } from 'next-auth/react'
-// import { useRouter } from 'next/navigation'
-// import { useState } from 'react'
-// import axios from 'axios'
 
 const CreatePrompt = ({}) => {
-  // const router = useRouter()
-  // const { data: session } = useSession()
-
-  // const [submitting, setSubmitting] = useState(false)
-  // const [post, setPost] = useState({
-  //   prompt: '',
-  //   tag: '',
-  // })
-
-  // if (!session?.user) {
-  //   return router.push('/')
-  // }
-
-  // const createPrompt = async (e) => {
-  //   e.preventDefault()
-  //   setSubmitting(true)
-
-  //   try {
-  //     const response = await fetch('/api/prompt/new', {
-  //       method: 'POST',
-  //       body: JSON.stringify({
-  //         prompt: post.prompt,
-  //         userId: session?.user.id,
-  //         tag: post.tag,
-  //       }),
-  //     })
-
-  //     if (response.ok) {
-  //       router.push('/')
-  //     }
-  //   } catch (error) {
-  //     console.log(error)
-  //   } finally {
-  //     setSubmitting(false)
-  //   }
-  // }
-
   return <Form type='Create' />
 }
 
