45b3138254b6a6c39e0741e3a64f236bea4e3885
Sushil Gupta <kalwarsushil2@gmail.com>
2023-07-10 16:35:17 +0545

    bug fix: Prompt shown without been logged in

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,9 @@
 import Feed from '@components/Feed'
+import { useSession } from 'next-auth/react'
 
 const Home = () => {
+  const { data: session } = useSession()
+
   return (
     <section className='w-full flex-center flex-col'>
       <h1 className='head_text text-center'>
@@ -13,7 +16,7 @@ const Home = () => {
         discover, create and share creative prompts
       </p>
 
-      <Feed />
+      {session && <Feed />}
     </section>
   )
 }
