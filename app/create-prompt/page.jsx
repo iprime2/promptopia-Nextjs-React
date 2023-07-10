473e030746a48a81f585f735fadc81ee3d132381
Sushil Gupta <kalwarsushil2@gmail.com>
2023-07-10 16:07:22 +0545

    fixing pre render issue

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,58 +1,50 @@
 'use client'
 
 import Form from '@components/Form'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import axios from 'axios'
+// import { useSession } from 'next-auth/react'
+// import { useRouter } from 'next/navigation'
+// import { useState } from 'react'
+// import axios from 'axios'
 
 const CreatePrompt = ({}) => {
-  const router = useRouter()
-  const { data: session } = useSession()
+  // const router = useRouter()
+  // const { data: session } = useSession()
 
-  const [submitting, setSubmitting] = useState(false)
-  const [post, setPost] = useState({
-    prompt: '',
-    tag: '',
-  })
+  // const [submitting, setSubmitting] = useState(false)
+  // const [post, setPost] = useState({
+  //   prompt: '',
+  //   tag: '',
+  // })
 
-  if (!session?.user) {
-    return router.push('/')
-  }
+  // if (!session?.user) {
+  //   return router.push('/')
+  // }
 
-  const createPrompt = async (e) => {
-    e.preventDefault()
-    setSubmitting(true)
+  // const createPrompt = async (e) => {
+  //   e.preventDefault()
+  //   setSubmitting(true)
 
-    try {
-      const response = await fetch('/api/prompt/new', {
-        method: 'POST',
-        body: JSON.stringify({
-          prompt: post.prompt,
-          userId: session?.user.id,
-          tag: post.tag,
-        }),
-      })
+  //   try {
+  //     const response = await fetch('/api/prompt/new', {
+  //       method: 'POST',
+  //       body: JSON.stringify({
+  //         prompt: post.prompt,
+  //         userId: session?.user.id,
+  //         tag: post.tag,
+  //       }),
+  //     })
 
-      if (response.ok) {
-        router.push('/')
-      }
-    } catch (error) {
-      console.log(error)
-    } finally {
-      setSubmitting(false)
-    }
-  }
+  //     if (response.ok) {
+  //       router.push('/')
+  //     }
+  //   } catch (error) {
+  //     console.log(error)
+  //   } finally {
+  //     setSubmitting(false)
+  //   }
+  // }
 
-  return (
-    <Form
-      type='Create'
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={createPrompt}
-    />
-  )
+  return <Form type='Create' />
 }
 
 export default CreatePrompt
--- components/Form.jsx
+++ components/Form.jsx
@@ -3,7 +3,47 @@
 import Link from 'next/link'
 import React from 'react'
 
-const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+const Form = ({ type }) => {
+  const router = useRouter()
+  const { data: session } = useSession()
+
+  const [submitting, setSubmitting] = useState(false)
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  })
+
+  if (!session?.user) {
+    return router.push('/')
+  }
+
+  const createPrompt = async (e) => {
+    e.preventDefault()
+    setSubmitting(true)
+
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      })
+
+      if (response.ok) {
+        router.push('/')
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setSubmitting(false)
+    }
+  }
   return (
     <section className='w-full max-w-full flex-start flex-col'>
       <h1 className='head_text text-left'>
@@ -14,7 +54,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
         imagination run wild wth any AI-powered platform.
       </p>
       <form
-        onSubmit={handleSubmit}
+        onSubmit={createPrompt}
         className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
       >
         <label>
